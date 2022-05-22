5be061db10df85795a8d04c19faeca4c4b696bda
buğra arslan <aarslanbugra@outlook.com>
2022-05-22 20:24:25 +0300

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,7 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.body.localStorage.setItem('mode');
+    console.log('Page Loaded.');
+});
 document.getElementById("theme-btn").addEventListener(('click'), () => {
     document.body.classList.toggle("dark");
     localStorage.setItem('mode', document.body.classList);
