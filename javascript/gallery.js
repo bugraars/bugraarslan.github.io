232dd2803dfb1563261965dd40a675a1fafc320d
buğra arslan <aarslanbugra@outlook.com>
2022-05-22 20:04:55 +0300

    New Design

---
+++ javascript/gallery.js
@@ -0,0 +1,17 @@
+// const postPhotos = document.querySelector("#post-photos");
+// const postVideos = document.querySelector("#post-videos");
+
+//     function showVideos() {
+//         postVideos.classList.remove('none-display');
+//         postPhotos.classList.add('none-display');
+//     }
+//     function showPhotos() {
+//         postPhotos.classList.remove('none-display');
+//         postVideos.classList.add('none-display');
+//     }
+
+// const showMore = document.querySelector("#show-more");
+//     function showPost() {
+//         showMore.classList.add('none-display');
+        
+//     }
