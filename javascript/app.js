c0f877af54b06ff524a33acf98a790bf483a2e7f
buğra arslan <aarslanbugra@outlook.com>
2022-05-23 09:14:24 +0300

    bug fixed

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,26 @@
-document.getElementById("theme-btn").addEventListener(('click'), () => {
-    document.body.classList.toggle('dark');
-    localStorage.setItem('mode',document.body.classList);
+// document.getElementById("theme-btn").addEventListener(('click'), () => {
+//     document.body.classList.toggle('dark');
+//     localStorage.setItem('mode',document.body.classList);
+// });
+
+// if (localStorage.getItem('mode') != '') {
+//     document.body.classList.add(localStorage.getItem('mode'));
+// }
+
+const body = document.body;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('theme') == null) {
+        localStorage.setItem('theme', 'light');
+        body.classList.add("light");
+    }
+    else{
+        body.classList.add(localStorage.getItem('theme'));
+    }  
 });
 
-if (localStorage.getItem('mode') != '') {
-    document.body.classList.add(localStorage.getItem('mode'));
-}
-
+document.getElementById("theme-btn").addEventListener(('click'), () => {
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
+    localStorage.setItem('theme',document.body.classList);
+});
