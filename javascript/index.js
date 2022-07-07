167f22dd0772f25411c4d15c5b5f201dbf36f673
buğra arslan <aarslanbugra@outlook.com>
2022-07-07 04:46:25 +0300

    Page Loader Added

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,29 @@
 const aboutTitle = document.querySelector("#changing-text");
-        let texts = new Array();
-        texts.push(" a web developer <i class='fa-solid fa-code'></i>");
-        texts.push(" a photographer <i class='fa-solid fa-camera'></i>");
-        texts.push(" a music-lover <i class='fa-solid fa-music'></i>");
+let texts = new Array();
+texts.push(" a web developer <i class='fa-solid fa-code'></i>");
+texts.push(" a photographer <i class='fa-solid fa-camera'></i>");
+texts.push(" a music-lover <i class='fa-solid fa-music'></i>");
 
-        let point = 0;
-        function changeTitle() {
-            aboutTitle.innerHTML = `<h3>${texts[point]}</h3>`
-            if (point < (texts.length - 1)) {
-                point++;
-            } else {
-                point = 0;
-            }
-        }
-        setInterval(changeTitle, 1000); /*Call it here*/
-        changeTitle();
+let point = 0;
+function changeTitle() {
+    aboutTitle.innerHTML = `<h3>${texts[point]}</h3>`
+    if (point < (texts.length - 1)) {
+        point++;
+    } else {
+        point = 0;
+    }
+}
+setInterval(changeTitle, 1000); /*Call it here*/
+changeTitle();
+
+const loader = document.querySelector(".loader");
+const container = document.querySelector(".container")
+
+window.addEventListener('load', event => {
+    setTimeout(loadfunction, 1500);
+});
+
+function loadfunction() {
+    loader.style.display = 'none';
+    container.classList.remove('hidden');
+}
