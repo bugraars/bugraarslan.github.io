9674dc6c3a0c7bb3098c7b6315b6cab0b23a3f99
bugra.arslan <bugra.arslan@gtsfly.de>
2022-10-14 14:38:21 +0300

    New version

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-const aboutTitle = document.querySelector("#changing-text");
-        let texts = new Array();
-        texts.push(" BUĞRA ARSLAN");
-        texts.push(" A WEB DEVELOPER ");
+const body = document.body;
+const themeBtn = document.getElementById("theme-btn");
+const themeIcon = document.getElementById("theme-icon");
+const loading = document.querySelector("#loading");
+const loaded = document.querySelectorAll(".loaded")
+const aboutTitle = document.querySelector("#mainTitleText");
 
-        let point = 0;
-        function changeTitle() {
-            aboutTitle.innerHTML = `<h1>${texts[point]}</h1>`
-            if (point < (texts.length - 1)) {
-                point++;
-            } else {
-                point = 0;
-            }
-        }
-        setInterval(changeTitle, 1500); /*Call it here*/
-        changeTitle();
+let texts = new Array();
+texts.push(" BUĞRA ARSLAN");
+texts.push(" A WEB DEVELOPER ");
+
+let point = 0;
+function changeTitle() {
+    aboutTitle.innerHTML = `<h1>${texts[point]}</h1>`
+    if (point < (texts.length - 1)) {
+        point++;
+    } else {
+        point = 0;
+    }
+}
+setInterval(changeTitle, 1500); /*Call it here*/
+changeTitle();
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('theme') == null) {
+        localStorage.setItem('theme', 'light');
+        body.classList.add("light");
+    }
+    else {
+        body.classList.add(localStorage.getItem('theme'));
+    }
+});
+
+function setTheme() {
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
+    localStorage.setItem('theme', body.classList);
+};
+ window.addEventListener('load', event => {
+    setTimeout(removeLoader, 1500);
+    
+});
+function removeLoader(){
+    loading.style.display = 'none';
+}
+    
+// $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+// $(window).on('load', function(){
+//   setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+// });
+// function removeLoader(){
+//     $( "#loadingDiv" ).fadeOut(500, function() {
+//       // fadeOut complete. Remove the loading div
+//       $( "#loadingDiv" ).remove(); //makes page more lightweight 
+//   });  
+// }
